f8a35dbb354cdb94b4421aff039c315564d1b6fe
guanfu1 <70610390+guanfu1@users.noreply.github.com>
2022-05-18 13:50:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="www.youtube.com";
+        url.hostname="bai-piao-wang-zhe-iplc2.98848.xyz";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
